72c667db7c6fe1f37c6d76869d1b4bec65de281c
Shivansh <shivanshmishra092003@gmail.com>
2025-06-15 23:02:20 +0530

    front-end update

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavbarDemo from "./components/Navbar";
+
+
+function App() {
+  return (
+    <div>
+      <NavbarDemo />
+     
+    </div>
+  );
+}
+
+export default App;
